567b57b33eb9125cc7c10dd6ae33663728011234
Rafli Akhmad Baihaqi <baihaqirafli30@gmail.com>
2024-07-06 21:14:25 +0700

    Repair Deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,6 +9,7 @@ import cookieParser from "cookie-parser";
 import path from "path";
 import {v2 as cloudinary} from "cloudinary";
 
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -22,16 +23,17 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() =>
 // Create express instance
 const app = express();
 
-//Set up API body as json object
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
+//Set up API body as json object
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
 
 //Handle user routes
 app.use("/api/users", userRoutes);
