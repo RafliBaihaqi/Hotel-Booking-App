d09d68d4527c33f82c70237973c9e5a8f474cee7
Rafli Akhmad Baihaqi <baihaqirafli30@gmail.com>
2024-07-06 20:35:30 +0700

    Repair Deployments

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,47 +7,43 @@ import authRoutes from "./routes/auth";
 import myHotelsRoutes from "./routes/my-hotels"; // Import myHotelsRoutes module
 import cookieParser from "cookie-parser";
 import path from "path";
-import { v2 as cloudinary } from "cloudinary";
+import {v2 as cloudinary} from "cloudinary";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
-mongoose
-  .connect(process.env.MONGODB_CONNECTION_STRING as string)
-  .then(() =>
-    console.log("Connected to Database:", process.env.MONGODB_CONNECTION_STRING)
-  );
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => 
+  console.log("Connected to Database:", process.env.MONGODB_CONNECTION_STRING)
+);
 
 // Create express instance
 const app = express();
-const FRONTEND_URL = "" || process.env.FRONTEND_URL;
-const port = 5000 || "";
+
 //Set up API body as json object
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: FRONTEND_URL,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
-
-app.use(express.static(path.join(__dirname, "../../frontend/dist"))); //Serve static assets
 
 //Handle user routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/my-hotels", myHotelsRoutes);
 
-//Pass on any req to our url that are not endpoint and let the react-router-dom handle them
+//Pass on any req to our url that are not endpoint and let the react-router-dom
 app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
-});
+  res.sendFile(path.join(__dirname,"../../frontend/dist/index.html"));
+})
+;app.use(express.static(path.join(__dirname,"../../frontend/dist")));//Serve static assets
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
