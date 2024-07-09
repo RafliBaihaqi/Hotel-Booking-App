9ff5719ed18e28f7540d601163e5608aabbad258
Rafli Akhmad Baihaqi <baihaqirafli30@gmail.com>
2024-07-09 14:01:48 +0700

    Fixing search feature deployments

--- frontend/src/api-clients.ts
+++ frontend/src/api-clients.ts
@@ -2,7 +2,7 @@ import { SignInFormData } from "./pages/Signin";
 import { RegisterFormData } from "./pages/register";
 import { HotelSearchResponse, HotelType } from "../../backend/src/shared/types";
 
-const API_BASE_URL = "" || import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = "";
 //|| import.meta.env.VITE_API_BASE_URL
 export const register = async (formData: RegisterFormData) => {
   const response = await fetch(`${API_BASE_URL}/api/users/register`, {
