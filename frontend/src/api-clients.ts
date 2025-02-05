c9c282402d2727d88f3d0c189343a202b607965b
Rafli Baihaqi <baihaqirafli30@gmail.com>
2025-02-05 18:18:59 +0700

    fix backend code

--- frontend/src/api-clients.ts
+++ frontend/src/api-clients.ts
@@ -8,7 +8,7 @@ import {
 } from "../../backend/src/shared/types";
 import { BookingFormData } from "./forms/BookingForm/BookingForm";
 
-const API_BASE_URL = "" || import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 //
 
 export const fetchCurrentUser = async (): Promise<UserType> => {
